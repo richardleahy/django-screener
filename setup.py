e708eda9f38cebb5823aa816c7d2eabac85d822d
Richard Leahy <richardleahy@users.noreply.github.com>
2016-08-24 23:49:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-screener',
-    version='0.1.0b1',
+    version='0.1.1b1',
     packages=find_packages(),
     include_package_data=True,
     license='BSD 3-Clause',
